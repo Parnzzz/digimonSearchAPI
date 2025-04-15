559bc07c74ce3c426f505d39ae755497e65d1e7d
Parnzzz <114709549+Parnzzz@users.noreply.github.com>
2025-04-16 04:17:33 +0700

    Update page.jsx

--- src/app/DigimonData/page.jsx
+++ src/app/DigimonData/page.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+
 
 
 function DigimonData() {
